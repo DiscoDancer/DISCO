ee1e11ff1baf16b5040995d91d90b7e602986c9c
discodancer <vlad.rabot9ga@gmail.com>
2022-07-13 17:43:38 +0300

    + 1 step

--- samples/redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ samples/redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -1,37 +1,36 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import {useSelector} from 'react-redux'
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
 import {TimeAgo} from "./TimeAgo";
+import {selectPostById} from "./postsSlice";
 
-export const SinglePostPage = ({ match }) => {
-  const { postId } = match.params
+export const SinglePostPage = ({match}) => {
+    const {postId} = match.params
 
-  const post = useSelector(state =>
-    state.posts.find(post => post.id === postId)
-  )
+    const post = useSelector(state => selectPostById(state, postId))
 
-  if (!post) {
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
+
+    const date = (new Date()).toISOString();
+
     return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
+        <section>
+            <article className="post">
+                <h2>{post.title}</h2>
+                <p className="post-content">{post.content}</p>
+                <Link to={`/editPost/${post.id}`} className="button">
+                    Edit Post
+                </Link>
+                <PostAuthor/>
+                <TimeAgo timestamp={date}/>
+            </article>
+        </section>
     )
-  }
-
-  const date = (new Date()).toISOString();
-
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <p className="post-content">{post.content}</p>
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
-        <PostAuthor/>
-        <TimeAgo timestamp={date}/>
-      </article>
-    </section>
-  )
 }
