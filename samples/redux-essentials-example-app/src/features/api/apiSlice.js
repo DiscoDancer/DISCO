3448601632cdc8f6f68f976195a91d38501c7b4a
Vladislav Evtushenko <evtushenko.v@outlook.com>
2022-07-21 11:53:51 +0300

    +1 chapter

---
+++ samples/redux-essentials-example-app/src/features/api/apiSlice.js
@@ -0,0 +1,31 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+    tagTypes: ['Post'],
+    endpoints: builder => ({
+        getPosts: builder.query({
+            query: () => '/posts',
+            providesTags: ['Post']
+        }),
+        getPost: builder.query({
+            query: postId => `/posts/${postId}`
+        }),
+        addNewPost: builder.mutation({
+            query: initialPost => ({
+                url: '/posts',
+                method: 'POST',
+                body: initialPost
+            }),
+            invalidatesTags: ['Post']
+        })
+    })
+})
+
+export const {
+    useGetPostsQuery,
+    useGetPostQuery,
+    useAddNewPostMutation
+} = apiSlice
--- samples/redux-essentials-example-app/src/features/posts/SinglePostPage.js
+++ samples/redux-essentials-example-app/src/features/posts/SinglePostPage.js
@@ -1,36 +1,37 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
-import {Link} from "react-router-dom";
-import {PostAuthor} from "./PostAuthor";
-import {TimeAgo} from "./TimeAgo";
-import {selectPostById} from "./postsSlice";
+import { Link } from 'react-router-dom'
 
-export const SinglePostPage = ({match}) => {
-    const {postId} = match.params
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery } from '../api/apiSlice'
 
-    const post = useSelector(state => selectPostById(state, postId))
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
-    if (!post) {
-        return (
-            <section>
-                <h2>Post not found!</h2>
-            </section>
-        )
-    }
+export const SinglePostPage = ({ match }) => {
+    const { postId } = match.params
 
-    const date = (new Date()).toISOString();
+    const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
 
-    return (
-        <section>
+    let content
+    if (isFetching) {
+        content = <Spinner text="Loading..." />
+    } else if (isSuccess) {
+        content = (
             <article className="post">
                 <h2>{post.title}</h2>
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
+                </div>
                 <p className="post-content">{post.content}</p>
+                <ReactionButtons post={post} />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
                 </Link>
-                <PostAuthor/>
-                <TimeAgo timestamp={date}/>
             </article>
-        </section>
-    )
+        )
+    }
+
+    return <section>{content}</section>
 }
